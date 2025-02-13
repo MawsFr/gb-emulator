f0432c6f2714351d18fc650ede052d5866a37d53
Mustapha Nezzari <mnezzari.pro@gmail.com>
2025-02-13 12:34:41 +0100

    feat: implement basic registers

--- packages/gb-emulator/__tests__/registers.spec.ts
+++ packages/gb-emulator/__tests__/registers.spec.ts
@@ -1,6 +1,118 @@
-import { describe } from "vitest";
-import { Registers } from "@/registers"
+// based on : https://gbdev.io/pandocs/CPU_Registers_and_Flags.html#the-flags-register-lower-8-bits-of-af-register
+
+import { beforeEach, describe, expect, it } from "vitest";
+import { Registers } from "@/registers";
 
 describe(Registers, () => {
+  let registers: Registers;
 
+  beforeEach(() => {
+    registers = new Registers();
+  });
+
+  describe("8 bit registers", () => {
+    it("should have an A register", () => {
+      registers.setA(0x01);
+      expect(registers.aSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+
+    it("should have a B register", () => {
+      registers.setB(0x01);
+      expect(registers.bSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+
+    it("should have a C register", () => {
+      registers.setC(0x01);
+      expect(registers.cSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+
+    it("should have a D register", () => {
+      registers.setD(0x01);
+      expect(registers.dSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+
+    it("should have an E register", () => {
+      registers.setE(0x01);
+      expect(registers.eSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+
+    it("should have an H register", () => {
+      registers.setH(0x01);
+      expect(registers.hSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+
+    it("should have an L register", () => {
+      registers.setL(0x01);
+      expect(registers.lSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+
+    it("should have an F register", () => {
+      registers.setF(0x01);
+      expect(registers.fSlot)
+          .to.be.a("number")
+          .and.to.equal(0x01);
+    });
+  });
+
+  describe("16 bits registers", () => {
+    it("should have an AF register with A as high and F as low", () => {
+      registers.setA(0x01);
+      registers.setF(0x02);
+      expect(registers.afSlot)
+          .to.be.a("number")
+          .and.to.equal(0x0102);
+    });
+
+    it("should have a BC register with B as high and C as low", () => {
+      registers.setB(0x01);
+      registers.setC(0x02);
+      expect(registers.bcSlot)
+          .to.be.a("number")
+          .and.to.equal(0x0102);
+    });
+
+    it("should have a DE register with D as high and E as low", () => {
+      registers.setD(0x01);
+      registers.setE(0x02);
+      expect(registers.deSlot)
+          .to.be.a("number")
+          .and.to.equal(0x0102);
+    });
+
+    it("should have a HL register with H as high and L as low", () => {
+      registers.setH(0x01);
+      registers.setL(0x02);
+      expect(registers.hlSlot)
+          .to.be.a("number")
+          .and.to.equal(0x0102);
+    });
+
+    it("should have a SP register", () => {
+      registers.setSP(0x0102);
+      expect(registers.spSlot)
+          .to.be.a("number")
+          .and.to.equal(0x0102);
+    });
+
+    it("should have a PC register", () => {
+      registers.setPC(0x0102);
+      expect(registers.pcSlot)
+          .to.be.a("number")
+          .and.to.equal(0x0102);
+    });
+  });
 });
--- packages/gb-emulator/src/registers.ts
+++ packages/gb-emulator/src/registers.ts
@@ -1,3 +1,111 @@
-export class Registers {
+import { concatBytes } from "@mawsfr/binary-operations";
 
+export class Registers {
+  private _aSlot: number = 0;
+  private _bSlot: number = 0;
+  private _cSlot: number = 0;
+  private _dSlot: number = 0;
+  private _eSlot: number = 0;
+  private _hSlot: number = 0;
+  private _lSlot: number = 0;
+  private _fSlot: number = 0;
+
+  private _spSlot: number = 0;
+  private _pcSlot: number = 0;
+
+  get aSlot() {
+    return this._aSlot & 0xFF;
+  }
+
+  get bSlot() {
+    return this._bSlot & 0xFF;
+  }
+
+  get cSlot() {
+    return this._cSlot & 0xFF;
+  }
+
+  get dSlot() {
+    return this._dSlot & 0xFF;
+  }
+
+  get eSlot() {
+    return this._eSlot & 0xFF;
+  }
+
+  get hSlot() {
+    return this._hSlot & 0xFF;
+  }
+
+  get lSlot() {
+    return this._lSlot & 0xFF;
+  }
+
+  get fSlot() {
+    return this._fSlot & 0xFF;
+  }
+
+  get afSlot() {
+    return concatBytes(this.aSlot, this.fSlot);
+  }
+
+  get bcSlot() {
+    return concatBytes(this.bSlot, this.cSlot);
+  }
+
+  get deSlot() {
+    return concatBytes(this.dSlot, this.eSlot);
+  }
+
+  get hlSlot() {
+    return concatBytes(this.hSlot, this.lSlot);
+  }
+
+  get spSlot() {
+    return this._spSlot & 0xFFff;
+  }
+
+  get pcSlot() {
+    return this._pcSlot & 0xFFff;
+  }
+
+  setA(a: number) {
+    this._aSlot = a & 0xFF;
+  }
+
+  setF(f: number) {
+    this._fSlot = f & 0xFF;
+  }
+
+  setB(b: number) {
+    this._bSlot = b & 0xFF;
+  }
+
+  setC(c: number) {
+    this._cSlot = c & 0xFF;
+  }
+
+  setD(d: number) {
+    this._dSlot = d & 0xFF;
+  }
+
+  setE(e: number) {
+    this._eSlot = e & 0xFF;
+  }
+
+  setH(h: number) {
+    this._hSlot = h & 0xFF;
+  }
+
+  setL(l: number) {
+    this._lSlot = l & 0xFF;
+  }
+
+  setSP(sp: number) {
+    this._spSlot = sp & 0xFFFF;
+  }
+
+  setPC(pc: number) {
+    this._pcSlot = pc & 0xFFFF;
+  }
 }
