70053582ae86573c4a57b98bad1803a0bfa3531f
Mustapha Nezzari <mnezzari.pro@gmail.com>
2025-02-13 11:35:07 +0100

    test: add hexa notation in description

--- packages/gb-emulator/__tests__/memory.spec.ts
+++ packages/gb-emulator/__tests__/memory.spec.ts
@@ -8,7 +8,7 @@ describe(Memory, () => {
         memory = new Memory();
     });
 
-    it('should have a 64KB of 8 bits addresses from 0 to 7FFF', () => {
+    it('should have a 64KB of 8 bits addresses from 0x0 to 0x7FFF', () => {
         expect(memory.addresses)
             .to.be.an.instanceof(Uint8Array)
             .and.have.length(0x8000);
