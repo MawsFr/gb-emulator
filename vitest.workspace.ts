d11aacf93585f1e6c7798dc510d0e61ad7da5013
Mustapha Nezzari <mnezzari.pro@gmail.com>
2025-02-14 13:15:19 +0100

    fix: add binary-operations package build in ci

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,6 +3,5 @@ import { defineWorkspace } from "vitest/config";
 export default defineWorkspace([
   "./packages/gb-emulator/vitest.config.ts",
   "./packages/binary-operations/vitest.config.ts",
-  "./app/vitest.config.ts",
-  "./vitest.config.js",
+  "./app/vitest.config.ts"
 ]);
